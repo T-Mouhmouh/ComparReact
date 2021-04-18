928444815a39b6a6bc26797fe38db2072cc7b878
T-Mouhmouh <75667115+T-Mouhmouh@users.noreply.github.com>
2021-04-19 00:26:20 +0100

    connect service with a compenet

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
-import "../Style/css/Header.css";
+import React, { Component, useState } from "react";
+import "../Style/css/header.css";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "../Style/img/car.jpg";
-export const HeaderComponent = () => {
-  return (
-    <div class="Navbar  sticky-top">
-      <div className="leftSide">
-        <img className="menuimg" src={logo} />
-        <div className="Links">
-          <a>Home</a>
-          <a>Client</a>
-          <a>Contact Us</a>
+export class HeaderComponent extends Component {
+  render() {
+    return (
+      <div class="Navbar  sticky-top">
+        <div className="leftSide">
+          <img className="menuimg" src={logo} />
+          <div className="Links">
+            <a>Home</a>
+            <a>Client</a>
+            <a>Contact Us</a>
+          </div>
+          <button>open</button>
         </div>
-        <button>open</button>
-      </div>
 
-      <div className="rightSide">
-        <button type="button" class="loginbtn btn btn-success">
-          Login
-        </button>
+        <div className="rightSide">
+          <button type="button" class="loginbtn btn btn-success">
+            Login
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+export default HeaderComponent;
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,52 +1,74 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import "../Style/css/SearchComponent.css";
 import "bootstrap/dist/css/bootstrap.css";
-export const SearchComponent = () => {
-  return (
-    <div class="SearchComponentParent container">
-      <div class="SearchComponentCont row center">
-        <div class="MarqueSection col-5">
-          <select class="mdb-select md-form" searchable="Search here..">
-            <option value="" disabled selected>
-              Choose your Marque
-            </option>
-            <option value="1">Ferrari</option>
-            <option value="2">Ford</option>
-            <option value="3">Mercides</option>
-            <option value="3">Reunault</option>
-            <option value="3">Fiat</option>
-          </select>
+import CarService from "../services/CarService.js";
+
+export class SearchComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      marque: "",
+      model: "",
+      ville: "",
+    };
+  }
+
+  submitSearch = (e) => {
+    e.preventDefault();
+    console.log("heloo");
+    CarService.ShercheCar("porche", "207", "rabat");
+  };
+
+  render() {
+    let { marque } = this.state;
+    return (
+      <form onSubmit={this.submitSearch}>
+        <div class="SearchComponentParent container">
+          <div class="SearchComponentCont row center">
+            <div class="MarqueSection col-5">
+              <select class="mdb-select md-form" searchable="Search here..">
+                <option value="" disabled selected>
+                  Choose your Marque
+                </option>
+                <option value="1">Ferrari</option>
+                <option value="2">Ford</option>
+                <option value="3">Mercides</option>
+                <option value="3">Reunault</option>
+                <option value="3">Fiat</option>
+              </select>
+            </div>
+            <div class="ModelSection col-5">
+              <select class="mdb-select md-form" searchable="Search here..">
+                <option value="" disabled selected>
+                  Choose your Model
+                </option>
+                <option value="1">USA</option>
+                <option value="2">Germany</option>
+                <option value="3">France</option>
+                <option value="3">Poland</option>
+                <option value="3">Japan</option>
+              </select>
+            </div>
+            <div class="Citysection col-2">
+              <select class="mdb-select md-form" searchable="Search here..">
+                <option value="" disabled selected>
+                  Choose your City
+                </option>
+                <option value="1">USA</option>
+                <option value="2">Germany</option>
+                <option value="3">France</option>
+                <option value="3">Poland</option>
+                <option value="3">Japan</option>
+              </select>
+            </div>
+          </div>
+          <div className="SearchDiv">
+            <button class="SearchBtn btn btn-success">Srchbtn</button>
+          </div>
         </div>
-        <div class="ModelSection col-5">
-          <select class="mdb-select md-form" searchable="Search here..">
-            <option value="" disabled selected>
-              Choose your Model
-            </option>
-            <option value="1">USA</option>
-            <option value="2">Germany</option>
-            <option value="3">France</option>
-            <option value="3">Poland</option>
-            <option value="3">Japan</option>
-          </select>
-        </div>
-        <div class="Citysection col-2">
-          <select class="mdb-select md-form" searchable="Search here..">
-            <option value="" disabled selected>
-              Choose your City
-            </option>
-            <option value="1">USA</option>
-            <option value="2">Germany</option>
-            <option value="3">France</option>
-            <option value="3">Poland</option>
-            <option value="3">Japan</option>
-          </select>
-        </div>
-      </div>
-      <div className="SearchDiv">
-        <button type="button" class="SearchBtn btn btn-success">
-          Srchbtn
-        </button>
-      </div>
-    </div>
-  );
-};
+      </form>
+    );
+  }
+}
+export default SearchComponent;
