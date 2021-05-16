49f04046a530b6e426cf5316b079783a18c2c6cc
T-Mouhmouh <75667115+T-Mouhmouh@users.noreply.github.com>
2021-05-16 22:16:22 +0100

    n

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         //<CitySliderComponent />
       }
 
-      <div className="row center">
+      <div className="row ">
         <FpImgSlider />
         <StoreComponent />
       </div>
